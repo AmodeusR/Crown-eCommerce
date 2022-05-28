fcc555e86fe1f6582f923cbaee19dcefcdee4ed8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-05-28 20:48:37 -0300

    Created Redux base boilerplate

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { UserProvider } from "./contexts/user.context";
+import { store } from "./store/store";
+
 import { CartProvider } from "./contexts/cart.context";
 
 import "./styles/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <UserProvider>
-        <CartProvider>
+    <Provider store={store}>
+      <CartProvider>
+        <Router>
           <App />
-        </CartProvider>
-      </UserProvider>
-    </Router>
+        </Router>
+      </CartProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
