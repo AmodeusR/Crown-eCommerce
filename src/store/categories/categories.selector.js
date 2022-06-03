f67dd740bbc55735d4b6cf6088af5101bf1d1132
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-06-02 23:25:57 -0300

    Memoized Categories

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,2 +1,15 @@
-export const selectCategories = ({ categories }) => categories.categories;
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+const selectCategoriesMemo = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories 
+);
+
+export const selectCategories = createSelector(
+  [selectCategoriesMemo],
+  (categories) => categories
+);
+
 export const selectIsFetching = ({ categories }) => categories.isFetching;
