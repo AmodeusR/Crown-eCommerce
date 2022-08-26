81e0a62dcf84d565bc41e75589b7aa05cad17968
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-25 21:39:37 -0300

    Finished PaymentForm functionality

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button from "../Button/Button";
+import { useSelector } from "react-redux";
+import { selectCartTotal } from "../../store/cart/cart.selector";
 
 import "./payment-form.scss";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const cartTotal = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const handlePayment = async (e) => {
     e.preventDefault();
@@ -17,25 +23,28 @@ const PaymentForm = () => {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ amount: 10000 })
+      body: JSON.stringify({ amount: cartTotal * 100 })
     }).then(response => response.json());
 
     const { paymentIntent: { client_secret } } = response;
 
+    setIsProcessingPayment(true);
+    
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: "Amodeus R."
+          name: currentUser ? currentUser.displayName : "Guest"
         }
       }
     });
-
+    
     if (paymentResult.error) {
       alert(paymentResult.alert);
     } else if (paymentResult.paymentIntent.status === "succeeded") {
       alert("Payment sucessful");
     }
+    setIsProcessingPayment(false);
   }
 
   return (
@@ -43,7 +52,7 @@ const PaymentForm = () => {
       <form className="payment-form" onSubmit={(e) => handlePayment(e)}>
         <h2 className="payment-form__title">Card Payment</h2>
       <CardElement />
-      <Button title="Confirm payment" className="inverted" />
+      <Button title="Confirm payment" className="inverted" isLoading={isProcessingPayment} />
       </form>
     </div>
   );
